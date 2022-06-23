8fa893896c3e07e573ce331e2ed18a41f4a7dced
Abdullah Faruk Özden <faruk.ozden@tccb.gov.tr>
2022-06-23 18:48:27 +0300

    sh files

---
+++ allow.sh
@@ -0,0 +1,12 @@
+sudo chmod 777 /home/ft/packet_ft_1/packet_forwarder/reset_lgw.sh
+sudo chmod 777 /home/ft/packet_ft_2/packet_forwarder/reset_lgw.sh
+sudo chmod 777 /home/ft/packet_ft_3/packet_forwarder/reset_lgw.sh
+sudo chmod 777 /home/ft/packet_ft_4/packet_forwarder/reset_lgw.sh
+sudo chmod 777 /home/ft/packet_ft_5/packet_forwarder/reset_lgw.sh
+sudo chmod 777 /home/ft/packet_ft_6/packet_forwarder/reset_lgw.sh
+
+
+sudo chmod 777 /home/ft/clonemiddles.sh
+sudo chmod 777 /home/ft/runall.sh
+sudo chmod 777 /home/ft/runmiddle.sh
+sudo chmod 777 /home/ft/stopallpfs.php
---
+++ clonemiddles.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/Farukest/ftmiddle.git
+
+git clone https://github.com/Farukest/ftmiddle_for_incomes.git
