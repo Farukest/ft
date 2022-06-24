515b4cb72cb744286a5f5b98a463b6cedefd73b3
Abdullah Faruk Özden <faruk.ozden@tccb.gov.tr>
2022-06-24 19:54:04 +0300

    allow sh changed

--- allow.sh
+++ allow.sh
@@ -3,10 +3,16 @@ sudo chmod 777 /home/ft/packet_ft_2/packet_forwarder/reset_lgw.sh
 sudo chmod 777 /home/ft/packet_ft_3/packet_forwarder/reset_lgw.sh
 sudo chmod 777 /home/ft/packet_ft_4/packet_forwarder/reset_lgw.sh
 sudo chmod 777 /home/ft/packet_ft_5/packet_forwarder/reset_lgw.sh
-sudo chmod 777 /home/ft/packet_ft_6/packet_forwarder/reset_lgw.sh
 
 
 sudo chmod 777 /home/ft/clonemiddles.sh
+sudo chmod 777 /home/ft/updatemiddles.sh
+
 sudo chmod 777 /home/ft/runall.sh
 sudo chmod 777 /home/ft/runmiddle.sh
 sudo chmod 777 /home/ft/stopallpfs.php
+
+sudo chmod 777 /home/ft/logs/clearlogs.sh
+sudo chmod 777 /home/ft/logs/listened.log
+sudo chmod 777 /home/ft/logs/sent.log
+sudo chmod 777 /home/ft/logs/signals.log
